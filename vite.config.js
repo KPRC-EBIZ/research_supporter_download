17869bb235691ec5d530777657a5147074eab152
LEEYEJI0839 <foretell59@kprc.or.kr>
2026-07-09 09:26:03 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/research_supporter/",
+  plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    preserveSymlinks: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      preserveSymlinks: true,
+    },
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
